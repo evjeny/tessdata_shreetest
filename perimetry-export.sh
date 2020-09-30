4aadccae8bfe6e53cd914fdd3e8a934ea2b8ce71
evjeny <gkanafing@gmail.com>
2020-09-30 12:44:58 +0500

    add exporter for perimetry config

--- perimetry-export.sh
+++ perimetry-export.sh
@@ -3,3 +3,5 @@ lstmtraining \
 --continue_from tesstutorial/perimetry_from_full/perimetry_plus_checkpoint \
 --model_output perimetry.traineddata \
 --stop_training
+
+combine_tessdata -o perimetry_plus.traineddata perimetry_plus.lstm-number-dawg perimetry_plus.lstm-punc-dawg
